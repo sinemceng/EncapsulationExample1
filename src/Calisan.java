23ddf7154ac5e892e5f3e09e01ede05f449699f8
Sinem Özdemir <sinozdemir04@gmail.com>
2024-10-18 20:18:11 +0300

    Dosya Oluşturuldu

---
+++ src/Calisan.java
@@ -0,0 +1,42 @@
+public class Calisan {
+    private long id;
+    private String name;
+    private String surname;
+    private int salary;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        if (salary>=1000 && salary<=5000) {
+            this.salary = salary;
+        }
+    }
+}
+
+
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Calisan calisan = new Calisan();
+        calisan.setId(15);
+        System.out.println("Çalışanın id'si: "+ calisan.getId());
+         calisan.setSalary(3000);
+        System.out.println("çalışanın maaşı: "+ calisan.getSalary());
+
+    }
+}
